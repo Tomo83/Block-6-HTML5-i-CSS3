8646cbcb26360ab1c20a2a2914631619c57db742
Tomo83 <tomek.kurzacz@gmail.com>
2017-10-03 12:50:01 +0200

    files changed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-$(document).ready(function(){
-	$('.fa').click(function(){
-		$('.side-menu').toggleClass('active');
-		$('i.fa').toggleClass('fa-bars').toggleClass('fa-times');
-		open = false;
-	});
+$(document).ready(function () {
+    /*slider*/
+    var slideIndex = 0;
+    slider();
+
+    function slider() {
+        var i;
+        var slide = document.getElementsByClassName('slide');
+
+        for (i = 0; i < slide.length; i++) {
+            slide[i].style.display = 'none';
+        }
+
+        slideIndex++;
+        if (slideIndex > slide.length) {
+            slideIndex = 1;
+        }
+        slide[slideIndex - 1].style.display = 'block';
+        setTimeout(slider, 6000);
+    }
+
+    /*side menu*/
+    $('.fa').click(function () {
+        $('.side-menu').toggleClass('active');
+        $('i.fa').toggleClass('fa-bars').toggleClass('fa-times');
+        open = false;
+    });
 
 });
-	
